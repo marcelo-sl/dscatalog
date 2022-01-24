d8b41c5ba24685e516c728e7a014cff1149e0a95
marcelo-sl <mar.sousa2061@gmail.com>
2022-01-24 20:00:24 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -30,4 +30,12 @@ public class CategoryService {
 		CategoryDTO dto = new CategoryDTO(category); 
 		return dto;
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
 }
